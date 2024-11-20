abd390be6dd19f667db1c869b7a15bde221b8a8c
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-20 19:05:12 +0500

    .

--- Movie.cs
+++ Movie.cs
@@ -2,23 +2,23 @@ public class Movie
 {
     public string Title { get; set; } // Film nomi
     
-    public string Director { get; set; } // Rejissor ismi
+    public string Director { get; set; } 
     
-    public string Genre { get; set; } // Janr
+    public string Genre { get; set; } 
     
-    public int ReleaseYear { get; set; } // Chiqish yili
+    public int ReleaseYear { get; set; } 
     
-    public double Duration { get; set; } // Davomiyligi (soatlarda)
+    public double Duration { get; set; } 
     
-    public string Rating { get; set; } // Reyting (masalan, IMDb reytingi)
+    public string Rating { get; set; } 
     
-    public string Language { get; set; } // Film tili
+    public string Language { get; set; } 
     
-    public int IsOscarWinner { get; set; } // Oscar mukofoti olganmi
+    public int IsOscarWinner { get; set; } 
     
-    public string LeadActor { get; set; } // Bosh rol ijrochisi
+    public string LeadActor { get; set; } 
     
-    public string Budget { get; set; } // Byudjet (million dollar)
+    public string Budget { get; set; } 
 
     public void DisplayInfo()
     {
